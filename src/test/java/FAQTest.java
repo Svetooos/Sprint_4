291bdcf0b87bc9b0c4ec8f9414c3aa1a7ea28aa0
Svetlana Ozerova <ozerovas99@mail.ru>
2024-11-29 11:17:24 +0400

    Исправление замечаний

--- src/test/java/FAQTest.java
+++ src/test/java/FAQTest.java
@@ -1,4 +1,3 @@
-import util.Answers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,37 +7,45 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import page.HomePage;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static util.PageText.*;
 
 @RunWith(Parameterized.class)
 public class FAQTest {
 
     public WebDriver driver;
+    public JavascriptExecutor jsDriver;
 
     @Parameterized.Parameter()
-    public String browser;
+    public String buttonText;
+    @Parameterized.Parameter(value = 1)
+    public String answerText;
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{{"chrome"}, {"firefox"}});
-    }
 
     @Before
     public void initBrowser() {
-        if (browser.equals("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            driver = new ChromeDriver(options);
-        } else if (browser.equals("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            driver = new FirefoxDriver(options);
-        }
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        jsDriver = (JavascriptExecutor) driver;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {PAY_BUTTON, PAY_ANSWER},
+                {SCOOTERS_BUTTON, SCOOTERS_ANSWER},
+                {RENT_BUTTON, RENT_ANSWER},
+                {TODAY_BUTTON, TODAY_ANSWER},
+                {EXTEND_BUTTON, EXTEND_ANSWER},
+                {CHARGE_BUTTON, CHARGE_ANSWER},
+                {CANCEL_BUTTON, CANCEL_ANSWER},
+                {REGION_BUTTON, REGION_ANSWER}
+        });
     }
 
     @After
@@ -49,39 +56,16 @@ public class FAQTest {
 
     @Test
     public void checkFAQ() {
-        JavascriptExecutor jsDriver = (JavascriptExecutor) driver;
         // переход на страницу тестового приложения
         driver.get("https://qa-scooter.praktikum-services.ru/");
         HomePage homePage = new HomePage(driver);
         //Скрол до конца страницы
         jsDriver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-        String FAQ_ERROR_MESSAGE = "Ответ не соответствует ожидаемому результату или кнопка не найдена";
-
         //Раскрываем каждый вопрос и проверяем сходится ли ответ со страницы с ожидаемым значением
-        String payAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_pay(), Answers.PAY_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.PAY_ANSWER, payAnswer);
+        String answerFromPage = homePage.getFAQAnswer(buttonText, answerText);
+        assertEquals(FAQ_ERROR_MESSAGE, answerText, answerFromPage);
 
-        String scootersAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_scooters(), Answers.SCOOTERS_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.SCOOTERS_ANSWER, scootersAnswer);
-
-        String rentAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_rent(), Answers.RENT_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.RENT_ANSWER, rentAnswer);
-
-        String todayAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_today(), Answers.TODAY_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.TODAY_ANSWER, todayAnswer);
-
-        String extendAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_extend(), Answers.EXTEND_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.EXTEND_ANSWER, extendAnswer);
-
-        String chargeAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_charge(), Answers.CHARGE_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.CHARGE_ANSWER, chargeAnswer);
-
-        String cancelAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_cancel(), Answers.CANCEL_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.CANCEL_ANSWER, cancelAnswer);
-
-        String regionAnswer = homePage.getFAQAnswer(homePage.getButtonFAQ_region(), Answers.REGION_ANSWER);
-        assertEquals(FAQ_ERROR_MESSAGE, Answers.REGION_ANSWER, regionAnswer);
     }
 
 }
--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -6,8 +6,6 @@ import org.junit.runners.Parameterized;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import page.FinalOrderPage;
 import page.HomePage;
 import page.OderPage;
@@ -32,51 +30,43 @@ public class OrderTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"chrome", "Тест", "Тестов", "ул. Прокатная", "79994391022", "Театральная", "20.11.2024", "трое суток", "grey", "Коммент"},
-                {"firefox", "Иван", "Иванов", "ул. Красная", "79123456712", "Строгино", "01.12.2024", "сутки", null, null}
+                {"Тест", "Тестов", "ул. Прокатная", "79994391022", "Театральная", "20.11.2024", "трое суток", "grey", "Коммент"},
+                {"Иван", "Иванов", "ул. Красная", "79123456712", "Строгино", "01.12.2024", "сутки", null, null}
         });
     }
 
-    @Parameterized.Parameter(value = 1)
+    @Parameterized.Parameter()
     public String name;
 
-    @Parameterized.Parameter(value = 2)
+    @Parameterized.Parameter(value = 1)
     public String lastName;
 
-    @Parameterized.Parameter(value = 3)
+    @Parameterized.Parameter(value = 2)
     public String address;
 
-    @Parameterized.Parameter(value = 4)
+    @Parameterized.Parameter(value = 3)
     public String phone;
 
-    @Parameterized.Parameter(value = 5)
+    @Parameterized.Parameter(value = 4)
     public String subway;
 
-    @Parameterized.Parameter(value = 6)
+    @Parameterized.Parameter(value = 5)
     public String deliveryDate;
 
-    @Parameterized.Parameter(value = 7)
+    @Parameterized.Parameter(value = 6)
     public String rent;
 
-    @Parameterized.Parameter(value = 8)
+    @Parameterized.Parameter(value = 7)
     public String color;
 
-    @Parameterized.Parameter(value = 9)
+    @Parameterized.Parameter(value = 8)
     public String comment;
-
-    @Parameterized.Parameter()
-    public String browser;
 
 
     @Before
     public void initBrowser() {
-        if (browser.equals("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            driver = new ChromeDriver(options);
-        } else if (browser.equals("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            driver = new FirefoxDriver(options);
-        }
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
     }
 
 
